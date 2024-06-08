cc7cdab83744c43c6f910e7df313d68cb0d177f7
oseeshogun <omasuaku@gmail.com>
2024-06-09 00:35:51 +0100

    :sparkles: feat(about): Add  informations about the current work code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.oseemasuaku.codedutravail"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
+        versionCode = 7
         versionName = "1.0"
         multiDexEnabled = true
 
--- app/src/main/java/com/oseemasuaku/codedutravail/MainActivity.kt
+++ app/src/main/java/com/oseemasuaku/codedutravail/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.oseemasuaku.codedutravail.presentation.screens.article.ArticleScreen
 import com.oseemasuaku.codedutravail.presentation.screens.home.Home
+import com.oseemasuaku.codedutravail.presentation.screens.information.AboutWorkCode
 import com.oseemasuaku.codedutravail.presentation.screens.information.InformationScreen
 import com.oseemasuaku.codedutravail.presentation.screens.search.ArticleSearch
 import com.oseemasuaku.codedutravail.ui.theme.CodeDuTravailTheme
@@ -27,13 +28,20 @@ class MainActivity : ComponentActivity() {
                         Home(navController)
                     }
                     composable("article/{id}") { navBackStackEntry ->
-                        navBackStackEntry.arguments?.let { ArticleScreen(id = it.getString("id")!!.toInt()) }
+                        navBackStackEntry.arguments?.let {
+                            ArticleScreen(
+                                id = it.getString("id")!!.toInt()
+                            )
+                        }
                     }
                     composable("search") {
                         ArticleSearch(navController)
                     }
                     composable("info") {
                         InformationScreen(navController)
+                    }
+                    composable("about") {
+                        AboutWorkCode(navController)
                     }
                 }
             }
