d329a0876117b7da8d80a80a05a4eb32330102fd
oseeshogun <omasuaku@gmail.com>
2025-05-25 12:39:15 +0100

    fix: remove duplicate codedutravail in android namespace and application ID

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 }
 
 android {
-    namespace = "com.oseemasuaku.codedutravail.codedutravail"
+    namespace = "com.oseemasuaku.codedutravail"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973" // flutter.ndkVersion
     
@@ -43,7 +43,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.oseemasuaku.codedutravail.codedutravail"
+        applicationId = "com.oseemasuaku.codedutravail"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23 // flutter.minSdkVersion
