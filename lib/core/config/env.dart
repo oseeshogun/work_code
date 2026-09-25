0a349149dba86aa32c8a4a0d1bb243ce8278945c
oseeshogun <omasuaku@gmail.com>
2026-09-25 01:34:49 +0100

    feat: add support for building IPA and update ad unit IDs for iOS

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -1,4 +1,12 @@
+import 'dart:io';
+
 abstract class Env {
-  static const String articleBannerAdUnitId = String.fromEnvironment('articleBannerAdUnitId');
-  static const String appOpenAdUnitId = String.fromEnvironment('appOpenAdUnitId');
+  static const String _articleBannerAdUnitIdAndroid = String.fromEnvironment('articleBannerAdUnitIdAndroid');
+  static const String _articleBannerAdUnitIdIos = String.fromEnvironment('articleBannerAdUnitIdIos');
+  static const String _appOpenAdUnitIdAndroid = String.fromEnvironment('appOpenAdUnitIdAndroid');
+  static const String _appOpenAdUnitIdIos = String.fromEnvironment('appOpenAdUnitIdIos');
+
+  static String get articleBannerAdUnitId => Platform.isIOS ? _articleBannerAdUnitIdIos : _articleBannerAdUnitIdAndroid;
+
+  static String get appOpenAdUnitId => Platform.isIOS ? _appOpenAdUnitIdIos : _appOpenAdUnitIdAndroid;
 }
